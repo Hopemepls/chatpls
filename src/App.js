a63c40c86762f84786207286f96d3d090b9f708f
Denis <loostmepls@gmail.com>
2025-10-03 19:29:33 +0300

    add

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Chat from './chat';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Chat/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
